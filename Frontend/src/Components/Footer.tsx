c540d27669c7fbeeb28d1bfe7588296e8ac21b14
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-01-14 12:08:53 +0530

    Update Footer.tsx

--- Frontend/src/Components/Footer.tsx
+++ Frontend/src/Components/Footer.tsx
@@ -1,9 +1,15 @@
 import { Box, Typography } from "@mui/material";
 
-function Footer() {
+function Footer({ isDarkThemeOn }: { isDarkThemeOn: boolean }) {
   return (
-    <Box width={"fit-parent"} height={"30px"} textAlign={"center"} padding={1}>
-      <Typography variant="caption">
+    <Box
+      width={"fit-parent"}
+      height={"30px"}
+      textAlign={"center"}
+      alignContent={"center"}
+      padding={1}
+    >
+      <Typography variant="caption" color={isDarkThemeOn ? "white" : "black"}>
         All rights reserved © 2025 PeraBytes
       </Typography>
     </Box>
