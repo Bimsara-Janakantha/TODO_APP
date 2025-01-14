aedc79675d963ab732f1c1467579516ac83a8527
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-01-14 17:35:14 +0530

    Update models.ts

--- Backend/src/models.ts
+++ Backend/src/models.ts
@@ -1,12 +1,6 @@
-import { InferSchemaType, model, Schema } from "mongoose";
-
-const adminSchema = new Schema({
-  avatar: { type: String, required: false },
-  name: { type: String, required: true },
-  role: { type: String, required: true },
-  org: { type: String, required: true },
-});
-
-type Admin = InferSchemaType<typeof adminSchema>;
-
-export default model<Admin>("admins", adminSchema); // (<collection_name_in_plural>, <schema_name>)
+export interface Todo {
+  id: string;
+  title: string;
+  content: string;
+  dueDate: string;
+}
