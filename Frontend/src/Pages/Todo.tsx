7ea5735edf8d1437be781b2aa973e8e711912108
Bimsara Janakantha <147314704+Bimsara-Janakantha@users.noreply.github.com>
2025-01-14 18:30:00 +0530

    Update Todo.tsx

--- Frontend/src/Pages/Todo.tsx
+++ Frontend/src/Pages/Todo.tsx
@@ -5,41 +5,6 @@ import CreateNew from "../Components/NewTodo";
 import { addData, deleteData, getData } from "../Api/Api";
 import { useNavigate } from "react-router-dom";
 
-/* const TODOs = [
-  {
-    id: "1",
-    title: "Complete Project Proposal",
-    content: "Finish the draft and review it with the team by today evening.",
-    dueDate: "2025-01-15",
-  },
-  {
-    id: "2",
-    title: "Team Meeting",
-    content:
-      "Discuss project milestones and delegate tasks for the next sprint.",
-    dueDate: "2025-01-16",
-  },
-  {
-    id: "3",
-    title: "Buy Groceries",
-    content: "Purchase vegetables, milk, and other essentials for the week.",
-    dueDate: "2025-01-17",
-  },
-  {
-    id: "4",
-    title: "Workout Session",
-    content: "Attend the gym at 6 AM for strength training exercises.",
-    dueDate: "2025-01-18",
-  },
-  {
-    id: "5",
-    title: "Call with Client",
-    content:
-      "Discuss project requirements and clarify deliverables for the next phase.",
-    dueDate: "2025-01-19",
-  },
-]; */
-
 interface TODOProps {
   id: string;
   title: string;
@@ -59,10 +24,10 @@ function Todo() {
   const getEvent = async () => {
     try {
       const serverResposnse = await getData("todos");
-      console.log("Todo List: ", serverResposnse.data);
+      //console.log("Todo List: ", serverResposnse.data);
       setTodos(serverResposnse.data);
     } catch (error) {
-      console.log("Error occured");
+      //console.log("Error occured");
       console.log("Error in fetching todo list!", error);
     }
   };
